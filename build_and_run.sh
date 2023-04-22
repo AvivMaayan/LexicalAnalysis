be678c3491173272f69fac8131c8eb8ba5d95fa7
nitai.kluger <nitai.kluger@campus.technion.ac.il>
2023-04-22 13:33:57 +0300

    changed to general tests

--- build_and_run.sh
+++ build_and_run.sh
@@ -5,12 +5,12 @@ flex scanner.lex
 g++ lex.yy.c hw1.cpp -o hw1.out
 echo "*********build was successful************"
 
-# run t1
-echo "*****************run t1******************"
-./hw1.out < tests_in/t1.in >& tests_out/t1.out
-diff tests_out/t1.out tests_out_exp/t1.out
-
-# run t2
-echo "*****************run t2******************"
-./hw1.out < tests_in/t2.in >& tests_out/t2.out
-diff tests_out/t2.out tests_out_exp/t2.out
+cd tests_in
+for TEST in *.in 
+do 
+name=${TEST%.in} # remove the .in at the end
+echo "*****************run $name******************"
+../hw1.out < $TEST >& ../tests_out/$name.out
+diff ../tests_out/$name.out ../tests_out_exp/$name.out 
+done
+cd ../
